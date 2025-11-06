5eea87424aca53adef490a73e900079962e48e6a
PIASH <zmnobab1999@gmail.com>
2025-11-06 22:48:16 +0600

    mutual friend count solving with slow method

---
+++ M04P02.js
@@ -0,0 +1,40 @@
+// Problem Statement
+
+// You are given two large arrays, listA and listB. each array contains user objects.
+// A user object is guaranteed to have a unique id property ( a string) and may contain other data, such as a name.
+
+// Your task is to write an efficient function that take both lists as input
+// and returns the total count of users that are present in both lists.
+
+// ! Do not change anything in data setup part
+
+// --------------Data setup block--------------//
+const USER_COUNT = 50000;
+let usersA = [];
+let usersB = [];
+const createUser = (id) => ({ id: `user_${id}`, name: `user ${id}` });
+for (let i = 0; i < USER_COUNT; i++) {
+  usersA.push(createUser(i));
+  usersB.push(createUser(i + 25000));
+}
+
+// users 25000  to 49999 are common (25,000 common users)
+// --------------Data setup block--------------//
+// ---ALGORITHMS---//
+
+// Solving with slow method
+
+const commonFriendsSlow = (usersA, usersB) => {
+  const startTime = performance.now();
+  const commonFriends = [];
+  usersA.forEach((userA) => {
+    usersB.forEach((userB) => {
+      if (userA.id === userB.id) {
+        commonFriends.push(userA);
+      }
+    });
+  });
+  const endTime = performance.now();
+  return { count: commonFriends.length, timeTookToFinish: endTime - startTime };
+};
+console.log(commonFriendsSlow(usersA, usersB))
