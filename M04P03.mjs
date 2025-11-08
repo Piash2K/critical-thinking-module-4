b9ae6dcfa3ebd25078fa38a82ae781b6e8cccdf3
PIASH <zmnobab1999@gmail.com>
2025-11-08 15:08:06 +0600

    valid parentheses

---
+++ M04P03.mjs
@@ -0,0 +1,39 @@
+import Stack from "./stack.mjs";
+
+// Problem Statement
+
+// Given a sting s  containing just the characters "(",")","{","}","[","]",
+// Determine if the input sting is valid
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+//? Input and Output
+//? "()[]{}"=> true
+//? "([{}])"=> true
+//? "(]"=> false
+//? "(()"=>false
+
+const bracketChecker = (str) => {
+  const stack = new Stack();
+
+  const bracketMap = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    // console.log(element)
+    if (char === "(" || char === "{" || char === "[") {
+      stack.push(char);
+    } else if (char === ")" || char === "}" || char === "]") {
+      if (stack.isEmpty() || stack.pop() !== bracketMap[char]) {
+        return false;
+      }
+    }
+  }
+  return stack.isEmpty();
+};
+console.log(bracketChecker("(()[]]"));
